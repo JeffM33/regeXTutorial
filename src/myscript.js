f8a4d8188fabcdd050c1be505e3fe160c6812bb4
Jeffrey McLeod <jeffreymmcleod@outlook.com>
2021-09-23 10:19:41 -0500

    commiting all changes

--- src/myscript.js
+++ src/myscript.js
@@ -1,3 +1,9 @@
 let validEmail = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
-
+let testEmail = 'abc@email.com';
 // https://www.youtube.com/watch?v=u6Eesv7aqHo&ab_channel=CoolITHelp
+
+let validURL = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+
+
+
+RegExp.test.testEmail;
